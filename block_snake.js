b42561d7caa956b898a980b1a5c80ddd6c973edc
roland <rolandblok@gmail.com>
2021-12-16 11:22:42 +0100

    background save bug

--- block_snake.js
+++ block_snake.js
@@ -44,10 +44,11 @@ class block_snake {
         }
         
         p.clear()
-        p.stroke(bgc) 
-        p.fill(bgc)
-        p.rect(0,0,w,h)                 // make sure there is no transparant: movies will fail
-
+        if (p.type === 'SCREEN') {
+            p.stroke(bgc) 
+            p.fill(bgc)
+            p.rect(0,0,w,h)                 // make sure there is no transparant: movies will fail
+        }
         p.stroke(fgc) 
         p.noFill()
 
--- read_json.js
+++ read_json.js
@@ -106,10 +106,11 @@ class read_json {
         let Right = h
 
         p.clear()
-        p.stroke(bgc) 
-        p.fill(bgc)
-        p.rect(0,0,w,h)                 // make sure there is no transparant: movies will fail
-
+        if (p.type === 'SCREEN') {
+            p.stroke(bgc) 
+            p.fill(bgc)
+            p.rect(0,0,w,h)                 // make sure there is no transparant: movies will fail
+        }
         p.stroke(fgc) 
         p.noFill()
         
