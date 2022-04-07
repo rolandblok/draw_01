24280526784c61ac89a70d1af14d3228559258a0
roland <rolandblok@gmail.com>
2022-04-07 16:17:35 +0200

    niuew spul

--- multi_sinus.js
+++ multi_sinus.js
@@ -17,7 +17,8 @@ class multi_sinus extends Drawer {
         this.gui_folder_draw_options.add(this,'tilt_scale1').onChange(function (v) { cvs.draw() }).step(0.01).listen()
         this.gui_folder_draw_options.add(this,'tilt_scale2').onChange(function (v) { cvs.draw() }).step(0.01).listen()
         this.gui_folder_draw_options.add(this,'h_scale').onChange(function (v) { cvs.draw() }).step(0.01).listen()
-        
+        this.gui_folder_draw_options.add(this,'spiral').onChange(function (v) { cvs.draw() }).listen()
+        this.gui_folder_draw_options.add(this,'delay').onChange(function (v) { cvs.draw() }).listen()
 
         this.gui_folder_draw_options.add(this,'discretizatie').onChange(function (v) { cvs.draw() }).listen().min(10). step(10)
         this.gui_folder_draw_options.add(this,'randseed').onChange(function (v) { cvs.draw() }).step(1).listen()
@@ -26,6 +27,7 @@ class multi_sinus extends Drawer {
         this.gui_folder_defaults.add(this, 'setting1')
         this.gui_folder_defaults.add(this, 'setting2')
         this.gui_folder_defaults.add(this, 'setting3')
+        this.gui_folder_defaults.add(this, 'setting4')
         this.gui_folder_defaults.add(this, 'rando')
         if(sub_gui === ' 0_0'){
             this.gui_folder_defaults.open()
@@ -46,10 +48,13 @@ class multi_sinus extends Drawer {
         this.sinus_scale = 5
         this.delay_scale = 8 * Math.PI
         this.tilt_scale1 = 1
-        this.tilt_scale2 = 0.5
+        this.tilt_scale2 = 1
         this.S_scale1 = 1
         this.S_scale2 = 1
         this.h_scale = 0
+        this.spiral = true
+        this.delay = true
+        this.kader = false
 
         if(draw_now) cvs.draw()
     }
@@ -64,6 +69,7 @@ class multi_sinus extends Drawer {
         this.delay_scale = 14.8
         cvs.draw()
     }
+    
     setting3() {
         this.R1 = this.wh_min * 0.15
         this.R2 = this.wh_min * 0.45
@@ -73,6 +79,34 @@ class multi_sinus extends Drawer {
         this.no_sinusus = 46
         this.sinus_scale = 5.3
         this.delay_scale = 17.2
+        cvs.draw()
+    }
+    setting4() {
+        this.R1 = this.wh_min * 0.15
+        this.R2 = this.wh_min * 0.40
+        this.no_lines = 125
+        this.discretizatie = 250
+        this.randseed = 0
+        this.no_sinusus = 50
+        this.sinus_scale = 5
+        this.sinus_scale = 5.3
+        this.delay_scale = 25.1
+        this.tilt_scale1 = 1
+        this.tilt_scale2 = 1  
+        cvs.draw()
+    }
+    setting5() {
+        this.R1 = 255
+        this.R2 = 290
+        this.no_lines = 60
+        this.discretizatie = 1250
+        this.randseed = 84
+        this.no_sinusus = 50
+        this.sinus_scale = 5
+        this.sinus_scale = 5.3
+        this.delay_scale = 25.1
+        this.tilt_scale1 = 1
+        this.tilt_scale2 = 1  
         cvs.draw()
     }
 
@@ -108,23 +142,60 @@ class multi_sinus extends Drawer {
             Ss_and_Phases_and_Freqs_and_Delays[i][3] = p.random() * this.delay_scale / (this.no_lines)
         }
 
-        let h_step   = this.h/2 * this.h_scale / this.no_lines
-        let h_offset = h_step * this.no_lines / 2
-        
-        for(let l_no = 0; l_no < this.no_lines; l_no++){
-            let R = this.R1  + l_no * (this.R2 - this.R1) / this.no_lines
-            let tilt_scale = this.tilt_scale1 + l_no * (this.tilt_scale2 - this.tilt_scale1) / this.no_lines
-            let S_scale = this.S_scale1 + l_no * (this.S_scale2 - this.S_scale1) / this.no_lines
-            for(let i = 0; i< this.no_sinusus; i++) {
-                Ss_and_Phases_and_Freqs_and_Delays[i][1] += Ss_and_Phases_and_Freqs_and_Delays[i][3]
+        if (!this.spiral) {
+            let h_step   = this.h/2 * this.h_scale / this.no_lines
+            let h_offset = h_step * this.no_lines / 2
+            
+            for(let l_no = 0; l_no < this.no_lines; l_no++){
+                let R = this.R1  + l_no * (this.R2 - this.R1) / this.no_lines
+                let tilt_scale = this.tilt_scale1 + l_no * (this.tilt_scale2 - this.tilt_scale1) / this.no_lines
+                let S_scale = this.S_scale1 + l_no * (this.S_scale2 - this.S_scale1) / this.no_lines
+                if (this.delay){
+                    for(let i = 0; i< this.no_sinusus; i++) {
+                        Ss_and_Phases_and_Freqs_and_Delays[i][1] += Ss_and_Phases_and_Freqs_and_Delays[i][3]
+                    }
+                }
+                p.beginShape()
+                for (let phi = 0; phi <= p.TWO_PI + FLOATING_POINT_ACCURACY; phi += (p.TWO_PI/this.discretizatie)) {
+
+
+                    let V = this.my_circle_sinusus(R, Ss_and_Phases_and_Freqs_and_Delays, phi, S_scale = S_scale, tilt_scale = tilt_scale)
+                    this.vertex_middle(p, V[X], V[Y] - h_offset + l_no * h_step)
+                    no_vertices ++
+                }
+                p.endShape()
             }
+        } else {
+            let h_step   = this.h/2 * this.h_scale / this.no_lines
+            let h_offset = h_step * this.no_lines / 2
+            
             p.beginShape()
-            for (let phi = 0; phi <= p.TWO_PI + FLOATING_POINT_ACCURACY; phi += (p.TWO_PI/this.discretizatie)) {
+            let R = this.R1
+            let S_scale = this.S_scale1
+            for(let l_no = -1; l_no < this.no_lines+1; l_no++){
+                let first_last = 1
+                if ((l_no == -1) || (l_no == this.no_lines)) {
+                    first_last = 0
+                }
+                let R_circle_step = (this.R2 - this.R1) / this.no_lines 
+                // let R = this.R1  + l_no * R_circle_step
+                let tilt_scale = this.tilt_scale1 + l_no * (this.tilt_scale2 - this.tilt_scale1) / this.no_lines
+                let S_scale_step = (this.S_scale2 - this.S_scale1) / this.no_lines
+                // let S_scale = this.S_scale1 + l_no * S_scale_step
 
-
-                let V = this.my_circle_sinusus(R, Ss_and_Phases_and_Freqs_and_Delays, phi, S_scale = S_scale, tilt_scale = tilt_scale)
-                this.vertex_middle(p, V[X], V[Y] - h_offset + l_no * h_step)
-                no_vertices ++
+                for (let phi = 0; phi <= p.TWO_PI + FLOATING_POINT_ACCURACY; phi += (p.TWO_PI/this.discretizatie)) {
+                    R += first_last*R_circle_step / (this.discretizatie+1)
+                    S_scale += first_last*S_scale_step / (this.discretizatie+1)
+                    if (this.delay) {
+                        for(let i = 0; i< this.no_sinusus; i++) {
+                            Ss_and_Phases_and_Freqs_and_Delays[i][1] += first_last * Ss_and_Phases_and_Freqs_and_Delays[i][3]/ (this.discretizatie+1)
+                        }
+                    }
+    
+                    let V = this.my_circle_sinusus(R, Ss_and_Phases_and_Freqs_and_Delays, phi, S_scale = S_scale, tilt_scale = tilt_scale)
+                    this.vertex_middle(p, V[X], V[Y] - h_offset + l_no * h_step)
+                    no_vertices ++
+                }
             }
             p.endShape()
         }
